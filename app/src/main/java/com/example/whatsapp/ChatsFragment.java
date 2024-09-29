2122c6e22adef0a4e907dd349fea5d07ce079856
chris001002 <chris001002@gmail.com>
2024-09-29 17:07:07 +0700

    First commit

---
+++ app/src/main/java/com/example/whatsapp/ChatsFragment.java
@@ -0,0 +1,92 @@
+package com.example.whatsapp;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatsFragment extends Fragment {
+    private class AdapterForRecyclerView extends RecyclerView.Adapter<RecyclerViewHolder> {
+        Context context;
+        List<Item> items;
+
+        public AdapterForRecyclerView(Context context, List<Item> items) {
+            this.context = context;
+            this.items = items;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new RecyclerViewHolder(LayoutInflater.from(context).inflate(R.layout.chat_cover,parent,false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
+            holder.name.setText(items.get(position).name);
+            holder.last_message.setText(items.get(position).lastMessage);
+            holder.imageView.setImageResource(items.get(position).image);
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+    }
+    private class Item {
+        String name;
+        String lastMessage;
+        int image;
+
+        public Item(String name, String lastMessage, int image) {
+            this.name = name;
+            this.lastMessage = lastMessage;
+            this.image = image;
+        }
+    }
+    private class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        ShapeableImageView imageView;
+        TextView name,last_message;
+        public RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.profile);
+            name = itemView.findViewById(R.id.user_name);
+            last_message = itemView.findViewById(R.id.last_message);
+        }
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chats, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("Jeff","Last message",R.drawable.user));
+        items.add(new Item("Test","Last message",R.drawable.user));
+        items.add(new Item("Test2","Last message",R.drawable.user));
+        items.add(new Item("Test3","Last message",R.drawable.user));
+        items.add(new Item("Jeff","Last message",R.drawable.user));
+        items.add(new Item("Test","Last message",R.drawable.user));
+        items.add(new Item("Test2","Last message",R.drawable.user));
+        items.add(new Item("Test3","Last message",R.drawable.user));
+        recyclerView.setAdapter(new AdapterForRecyclerView(getContext(),items));
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.whatsapp;
+
+import android.os.Bundle;
+import android.provider.CallLog;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private class ViewPagerAdapter extends FragmentStateAdapter{
+
+        public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position){
+                case 0: return new ChatsFragment();
+                case 1: return new StatusFragment();
+                case 2: return new CallsFragment();
+                default: return new ChatsFragment();
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager2 viewPager2 = findViewById(R.id.viewPager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(viewPagerAdapter);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setVisibility(View.VISIBLE);
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                    case 1:
+                    case 2:
+                        tabLayout.getTabAt(position).select();
+                }
+                super.onPageSelected(position);
+            }
+        });
+
+    }
+}
